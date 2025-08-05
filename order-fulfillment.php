666c626548a900cc54e946107b16b0b5ecffbccf
xophiaaa <xophiaanne.herrera@cvsu.edu.ph>
2025-08-05 11:42:14 +0800

    Update order-fulfillment.php

--- order-fulfillment.php
+++ order-fulfillment.php
@@ -30,7 +30,7 @@ include 'config/config.php';
             background: #f4f4f4;
             color: #000;
         }
-        .footer {
+        footer {
            position: fixed;
            left: 0;
            bottom: 0;
@@ -302,6 +302,7 @@ include 'config/config.php';
     </script>
 </body>
 </html>
+
 
 
 
